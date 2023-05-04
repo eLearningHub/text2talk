32ae872f8e40a9561e326de0d4acdbb664584e40
Behzad Samadi <behzad.samadi@gmail.com>
2023-05-04 17:46:00 -0400

    Removing extra line

--- install-text2talk.sh
+++ install-text2talk.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: Apache 2.0
 
 curl -o ~/.local/share/text2talk/define-text2talk.sh --create-dirs https://raw.githubusercontent.com/eLearningHub/text2talk/main/define-text2talk.sh 
-cp define-text2talk.sh ~/.local/share/text2talk
+
 echo "# Added by Text2Talk from eLearningHub: https://github.com/eLearningHub/text2talk" >> ~/.bashrc
 echo "source ~/.local/share/text2talk/define-text2talk.sh" >> ~/.bashrc
